a2c199ae4382030945d65c013549bc12bb37e8d9
jc <jchorzempa@posteo.org>
2023-01-30 23:04:09 +0100

    Add package json

--- learning_typescript/painter.ts
+++ learning_typescript/painter.ts
@@ -1,7 +1,12 @@
 interface Painter {
-  finish(): boolean;
-  ownMaterials: Material[];
-  paint(painting: string, materials: Material[]): boolean;
+	finish(): boolean;
+	// ownMaterials: Material[];
+	// paint(painting: string, materials: Material[]): boolean;
 }
 
-function paintPainting(painter: Painter, painting: string): boolean { return true}
+function paintPainting(painter: Painter, painting: string): boolean {
+	return true;
+}
+
+const artist = 'Augusta Savage';
+console.log({ artist });
