843359f4e9d39db93d94f3b096801010c5b48331
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-02 21:38:51 +0100

    Union types

--- learning-typescript/index.ts
+++ learning-typescript/index.ts
@@ -1,12 +1,12 @@
-//Union Types
+// Union Types
 // "Either-Or-Type" for variables that can belong to more than one type
 
-let mathematician: string | undefined = Math.random() > 0.5 ? undefined : 'Mark Wahlberg';
+const mathematician: string | undefined = Math.random() > 0.5 ? undefined : 'Mark Wahlberg';
 
-let physicist: string | number = Math.random() > 0.5 ? 'Marie Curie' : 84;
+const physicist: string | number = Math.random() > 0.5 ? 'Marie Curie' : 84;
 
-//Properties on Union Types
+// Properties on Union Types
 
-physicist.toString(); //is valid since it exists both on string and number
+physicist.toString(); // is valid since it exists both on string and number
 
-//physicist.toUpperCase(); //is not valid since it only exists on string
+// physicist.toUpperCase(); //is not valid since it only exists on string
