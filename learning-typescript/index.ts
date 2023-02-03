8f4abe265216f16ec531ae6277f372297476388e
jc <jchorzempa@posteo.org>
2023-02-03 22:53:13 +0100

    Add type narrowing

--- learning-typescript/index.ts
+++ learning-typescript/index.ts
@@ -10,3 +10,9 @@ const physicist: string | number = Math.random() > 0.5 ? 'Marie Curie' : 84;
 physicist.toString(); // is valid since it exists both on string and number
 
 // physicist.toUpperCase(); //is not valid since it only exists on string
+
+//Narrowing
+let admiral: number | string;
+admiral = 'Grace Hopper';
+admiral.toUpperCase();
+//admiral.toFixed() won't work anymore cause the variable was narrowed to be of type string
