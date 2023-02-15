76b7a674554048be21f3c18b35763ac08f336883
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-15 22:19:03 +0100

    Add optional parameter

--- learning-typescript/functions.ts
+++ learning-typescript/functions.ts
@@ -1,5 +1,16 @@
 //Passing any number of arguments to a function and typing them "regularly" makes them required arguments in TypeScript
 
-function (param1: number, param2: number): number {
-    return param1 + param2;
+function someFunction(param1: string, param2: number) {
+	return param1 + param2;
 }
+
+someFunction('hello', 1); //works fine
+someFunction('hello'); //throws type error
+
+//Adding the ? before the type annotation makes a parameter optional
+
+function anotherFunction(param1: string, param2?: string) {
+	return param1 + param2;
+}
+
+anotherFunction('hello'); //works fine
