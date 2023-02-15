723805597f9328061545e4294a26150b50a9e9c4
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-15 22:17:12 +0100

    Add required params

---
+++ learning-typescript/functions.ts
@@ -0,0 +1,5 @@
+//Passing any number of arguments to a function and typing them "regularly" makes them required arguments in TypeScript
+
+function (param1: number, param2: number): number {
+    return param1 + param2;
+}
